e14568046b263a11846327bf41aaf2b0b3f6fed8
jayasakthi <jayasakthi@sakthi>
2025-10-04 11:36:56 +0530

    commit

---
+++ Javaproject/src/day7/overloading/V1/Executor.java
@@ -0,0 +1,15 @@
+package day7.overloading.V1;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Is 121 Palindrome? "+Operatoions.isPalindrome(121));
+		System.out.println("Is 123 Palindrome? "+Operatoions.isPalindrome(123));
+		
+		System.out.println("Is CAR Palindrome? "+Operatoions.isPalindrome("CAR"));
+		System.out.println("Is RACECAR Palindrome? "+Operatoions.isPalindrome("RACECAR"));
+
+	}
+
+}
---
+++ Javaproject/src/day7/overriding/RBI.java
@@ -0,0 +1,7 @@
+package day7.overriding;
+
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+}
