a2dbe939b00422b9b9dac69fd3e1d56b493d5f0c
jayasakthi <jayasakthi@sakthi>
2025-10-04 11:02:28 +0530

    commit

---
+++ Javaproject/src/day6/usingfinal/FinalClassDemo.java
@@ -0,0 +1,13 @@
+package day6.usingfinal;
+final class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+}
+public class FinalClassDemo {
+	public static void main(String[] args) {
+		// Create the object of final class
+		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
+		f1.show();
+	}
+}
