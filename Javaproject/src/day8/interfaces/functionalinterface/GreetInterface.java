447a3f168c0e09bf7f6aee3e4bebf13321e00bff
jayasakthi <jayasakthi@sakthi>
2025-10-30 19:21:36 +0530

    commit

---
+++ Javaproject/src/day8/interfaces/functionalinterface/GreetInterface.java
@@ -0,0 +1,6 @@
+package day8.interfaces.functionalinterface;
+
+@FunctionalInterface
+public interface GreetInterface {
+	public String greet();
+}
---
+++ Javaproject/src/day9/StringSubClass.java
@@ -0,0 +1,9 @@
+package day9;
+
+public class StringSubClass extends String {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
