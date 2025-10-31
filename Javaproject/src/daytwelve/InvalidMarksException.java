29037432ab5626d28a7122898790149cb9a65222
jayasakthi <jayasakthi@sakthi>
2025-10-31 09:35:18 +0530

    commit

---
+++ Javaproject/src/daytwelve/InvalidMarksException.java
@@ -0,0 +1,14 @@
+package daytwelve;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
